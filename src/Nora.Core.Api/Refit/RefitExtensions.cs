f168e6a0fb95daf613e6c549c08a6284aedd46ba
Nora <wellington_nora94@hotmail.com>
2025-04-20 18:44:23 -0300

    feat: initial commit

---
+++ src/Nora.Core.Api/Refit/RefitExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using System.Text.Json;
+
+namespace Nora.Core.Api.Refit;
+
+public static class RefitExtensions
+{
+    public static IServiceCollection AddRefitClient<TClient>(
+        this IServiceCollection services,
+        string url) where TClient : class
+    {
+        var settings = new RefitSettings(new SystemTextJsonContentSerializer(new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));        
+
+        services            
+            .AddRefitClient<TClient>(settings)
+            .AddConfigurations(url);
+
+        return services;
+    }    
+
+    private static IHttpClientBuilder AddConfigurations(
+        this IHttpClientBuilder builder,
+        string url)
+    {
+        builder
+            .ConfigureHttpClient(configure => configure.BaseAddress = new Uri(url));
+
+        return builder;
+    }
+}
