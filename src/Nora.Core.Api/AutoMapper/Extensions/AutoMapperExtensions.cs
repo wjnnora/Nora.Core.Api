a770a01209c0bfbd270ef2e673fb5324f48c6157
Nora <wellington_nora94@hotmail.com>
2025-04-23 17:28:08 -0300

    feat: add some improvements

--- src/Nora.Core.Api/AutoMapper/Extensions/AutoMapperExtensions.cs
+++ src/Nora.Core.Api/AutoMapper/Extensions/AutoMapperExtensions.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Nora.Core.Api.AutoMapper.Extensions;
 
 public static class AutoMapperExtensions
 {
-    public static IServiceCollection AddAutoMapper<T1>(this IServiceCollection services)
+    public static IServiceCollection AddAutoMapper<T1>(this IServiceCollection services) where T1 : Profile
     {
         services.AddAutoMapper(typeof(T1).Assembly);
 
@@ -12,6 +13,8 @@ public static class AutoMapperExtensions
     }
 
     public static IServiceCollection AddAutoMapper<T1, T2>(this IServiceCollection services)
+        where T1 : Profile
+        where T2 : Profile
     {
         services.AddAutoMapper(typeof(T1).Assembly, typeof(T2).Assembly);
 
