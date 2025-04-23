75df2549a7fbc30d7b181bc6fec45aa30c8b3723
Nora <wellington_nora94@hotmail.com>
2025-04-23 16:59:59 -0300

    feat: add fluent validation

---
+++ src/Nora.Core.Api/FluentValidation/FluentValidationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Nora.Core.Api.FluentValidation;
+
+public static class FluentValidationExtensions
+{
+    public static IServiceCollection AddFluentValidation(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddValidatorsFromAssemblies(assemblies);        
+
+        return services;
+    }
+}
